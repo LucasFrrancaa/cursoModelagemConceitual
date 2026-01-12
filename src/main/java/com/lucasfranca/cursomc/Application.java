fac8563d0f6c500eafe0650db6b64d68d8caf030
lfranca <lfranca.digiboard.com.br>
2026-01-12 15:53:32 -0400

    Criando operacao de instanciacao

--- src/main/java/com/lucasfranca/cursomc/Application.java
+++ src/main/java/com/lucasfranca/cursomc/Application.java
@@ -1,13 +1,32 @@
 package com.lucasfranca.cursomc;
 
+import com.lucasfranca.cursomc.domain.Categoria;
+import com.lucasfranca.cursomc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception{
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
 	}
 
 }
