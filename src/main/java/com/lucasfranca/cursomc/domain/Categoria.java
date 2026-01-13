79cb7fc31a56540f719e97b79db9f97fc3a560c0
lfranca <lfranca.digiboard.com.br>
2026-01-13 10:01:53 -0400

    Produto e associacao muitos para muitos com Categoria

--- src/main/java/com/lucasfranca/cursomc/domain/Categoria.java
+++ src/main/java/com/lucasfranca/cursomc/domain/Categoria.java
@@ -2,20 +2,24 @@ package com.lucasfranca.cursomc.domain;
 
 import net.bytebuddy.dynamic.loading.InjectionClassLoader;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
 public class Categoria implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
+
+    @ManyToMany(mappedBy = "categorias")
+    private List<Produto> produtos = new ArrayList<>();
 
     public Categoria (){}
 
@@ -38,6 +42,14 @@ public class Categoria implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<Produto> produtos) {
+        this.produtos = produtos;
     }
 
     @Override
